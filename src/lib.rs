9743c824c399bfc283316f5ee72fcad43849657a
Anselm Schüler <mail@anselmschueler.com>
2023-01-29 15:09:45 +0100

    Document vec_map

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! This crate provides simple unoptimized [`Vec`]-based [sets] and [maps].
+//!
+//! [sets]: crate::vec_set::VecSet
+//! [maps]: crate::vec_map::VecMap
+
 pub mod vec_map;
 pub mod vec_set;
 
--- src/vec_map.rs
+++ src/vec_map.rs
@@ -1,61 +1,82 @@
+//! Associative maps, backed by a [`Vec`].
+
 use std::{borrow::Borrow, fmt::Debug, iter::FusedIterator};
 
+/// A simple associative map.
+/// It is a logic error for any key to change such that its equality
+/// under the [`Eq`] trait changes while it is in the map.
+/// To determine if two keys are “the same”, [`Eq`] is used.
 #[derive(Clone, PartialEq, Eq)]
 pub struct VecMap<K, V>(Vec<(K, V)>);
 
 impl<K, V> VecMap<K, V> {
+    /// Create a new [`VecMap`].
     pub fn new() -> VecMap<K, V> {
         VecMap(Vec::new())
     }
 
+    /// Create a new [`VecMap`] with a given pre-allocated capacity.
+    /// May allocate more than requested.
     pub fn with_capacity(capacity: usize) -> VecMap<K, V> {
         VecMap(Vec::with_capacity(capacity))
     }
 
+    /// Get the allocated capacity of the map.
     pub fn capacity(&self) -> usize {
         self.0.capacity()
     }
 
+    /// Get an iterator over the keys as references.
     pub fn keys(&self) -> Keys<'_, K, V> {
         Keys(self.iter())
     }
 
+    /// Get an iterator over the keys.
     pub fn into_keys(self) -> IntoKeys<K, V> {
         IntoKeys(self.into_iter())
     }
 
+    /// Get an iterator over the values as references.
     pub fn values(&self) -> Values<'_, K, V> {
         Values(self.iter())
     }
 
+    /// Get an iterator over the values as mutable references.
     pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
         ValuesMut(self.iter_mut())
     }
 
+    /// Get an iterator over the values.
     pub fn into_values(self) -> IntoValues<K, V> {
         IntoValues(self.into_iter())
     }
 
+    /// Get an iterator over the key-value pairs as tuples of reference.
     pub fn iter(&self) -> Iter<'_, K, V> {
         Iter(self.0.iter())
     }
 
+    /// Get an iterator over the key-value pairs as tuples of a reference and a mutable reference.
     pub fn iter_mut(&mut self) -> IterMut<'_, K, V> {
         IterMut(self.0.iter_mut())
     }
 
+    /// Get the amount of key-value pairs in the map.
     pub fn len(&self) -> usize {
         self.0.len()
     }
 
+    /// Check if the map is empty.
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
 
+    /// Empty the map and return an iterator over the cleared key-value pairs.
     pub fn drain(&mut self) -> Drain<'_, K, V> {
         Drain(self.0.drain(..))
     }
 
+    /// Remove all key-value pairs that do not satisfy a predicate.
     pub fn retain<F>(&mut self, mut f: F)
     where
         F: FnMut(&K, &mut V) -> bool,
@@ -64,14 +85,23 @@ impl<K, V> VecMap<K, V> {
         self.0.retain_mut(g)
     }
 
+    /// Remove all key-value pairs.
     pub fn clear(&mut self) {
         self.0.clear()
     }
 
+    /// Reserve additional space. May allocate more.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the new allocation fails.
     pub fn reserve(&mut self, additional: usize) {
         self.0.reserve(additional)
     }
 
+    /// Like [`try_reserve`], but returns a [`Result`] instead of panicking.
+    ///
+    /// [`try_reserve`]: std::collections::TryReserveError
     pub fn try_reserve(
         &mut self,
         additional: usize,
@@ -79,16 +109,21 @@ impl<K, V> VecMap<K, V> {
         self.0.try_reserve(additional)
     }
 
+    /// Shrink the capacity of the map as much as possible.
+    /// May keep more than precisely needed.
     pub fn shrink_to_fit(&mut self) {
         self.0.shrink_to_fit()
     }
 
+    /// Shrink the capacity of the map with a lower limit.
+    /// May keep more than precisely needed.
     pub fn shrink_to(&mut self, min_capacity: usize) {
         self.0.shrink_to(min_capacity)
     }
 }
 
 impl<K: Eq, V> VecMap<K, V> {
+    /// Returns an [`Entry`] for the given key.
     pub fn entry(&mut self, key: K) -> Entry<'_, K, V> {
         match self.0.iter().position(|(k, _)| *k == key) {
             Some(index) => Entry::Occupied(OccupiedEntry {
@@ -100,6 +135,7 @@ impl<K: Eq, V> VecMap<K, V> {
         }
     }
 
+    /// Get the given key’s value, if it exists.
     pub fn get<Q: ?Sized>(&self, k: &Q) -> Option<&V>
     where
         K: Borrow<Q>,
@@ -115,6 +151,7 @@ impl<K: Eq, V> VecMap<K, V> {
         result
     }
 
+    /// Get the given key’s key-value pair, if it exists.
     pub fn get_key_value<Q: ?Sized>(&self, k: &Q) -> Option<(&K, &V)>
     where
         K: Borrow<Q>,
@@ -130,6 +167,7 @@ impl<K: Eq, V> VecMap<K, V> {
         result
     }
 
+    /// Check if the map contains a given key.
     pub fn contains_key<Q: ?Sized>(&self, k: &Q) -> bool
     where
         K: Borrow<Q>,
@@ -145,6 +183,7 @@ impl<K: Eq, V> VecMap<K, V> {
         present
     }
 
+    /// Get a mutable reference to the given key’s value, if it exists.
     pub fn get_mut<Q: ?Sized>(&mut self, k: &Q) -> Option<&mut V>
     where
         K: Borrow<Q>,
@@ -160,6 +199,8 @@ impl<K: Eq, V> VecMap<K, V> {
         result
     }
 
+    /// Insert a key-value pair into the map.
+    /// If the key already exists, the old value is returned.
     pub fn insert(&mut self, k: K, v: V) -> Option<V> {
         match self.get_mut(&k) {
             Some(value) => Some(std::mem::replace(value, v)),
@@ -170,6 +211,8 @@ impl<K: Eq, V> VecMap<K, V> {
         }
     }
 
+    /// Remove the given key’s key-value pair from the map.
+    /// Returns the removed value, if it existed.
     pub fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
     where
         K: Borrow<Q>,
@@ -180,6 +223,8 @@ impl<K: Eq, V> VecMap<K, V> {
         Some(value)
     }
 
+    /// Remove the given key’s key-value pair from the map.
+    /// Returns the removed key-value pair, if it existed.
     pub fn remove_entry<Q: ?Sized>(&mut self, k: &Q) -> Option<(K, V)>
     where
         K: Borrow<Q>,
@@ -245,6 +290,12 @@ where
     Q: Eq,
 {
     type Output = V;
+
+    /// Get a given key’s corresponding value from the map.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the key is not in the map.
     fn index(&self, key: &Q) -> &Self::Output {
         self.get(key).expect("no entry found for key")
     }
@@ -269,11 +320,18 @@ impl<'a, K, V> IntoIterator for &'a mut VecMap<K, V> {
 impl<K, V> IntoIterator for VecMap<K, V> {
     type Item = (K, V);
     type IntoIter = IntoIter<K, V>;
+
+    /// Get an iterator over the key-value pairs.
     fn into_iter(self) -> Self::IntoIter {
         IntoIter(self.0.into_iter())
     }
 }
 
+/// An iterator over the keys of a map.
+/// Yields the keys as references.
+/// Generated by the [`keys`] method.
+///
+/// [`keys`]: VecMap::keys
 pub struct Keys<'a, K, V>(Iter<'a, K, V>);
 
 impl<K, V> Clone for Keys<'_, K, V> {
@@ -306,6 +364,11 @@ impl<'a, K, V> Iterator for Keys<'a, K, V> {
 
 impl<K, V> FusedIterator for Keys<'_, K, V> {}
 
+/// An iterator over the keys of a map.
+/// Yields the keys as values.
+/// Generated by the [`into_keys`] method.
+///
+/// [`into_keys`]: VecMap::into_keys
 pub struct IntoKeys<K, V>(IntoIter<K, V>);
 
 impl<K: Debug, V> Debug for IntoKeys<K, V> {
@@ -334,6 +397,11 @@ impl<K, V> Iterator for IntoKeys<K, V> {
 
 impl<K, V> FusedIterator for IntoKeys<K, V> {}
 
+/// An iterator over the values of a map.
+/// Yields the values as references.
+/// Generated by the [`values`] method.
+///
+/// [`values`]: VecMap::values
 pub struct Values<'a, K, V>(Iter<'a, K, V>);
 
 impl<K, V> Clone for Values<'_, K, V> {
@@ -366,6 +434,11 @@ impl<'a, K, V> Iterator for Values<'a, K, V> {
 
 impl<K, V> FusedIterator for Values<'_, K, V> {}
 
+/// An iterator over the values of a map.
+/// Yields the values as values.
+/// Generated by the [`values_mut`] method.
+///
+/// [`values_mut`]: VecMap::values_mut
 pub struct ValuesMut<'a, K, V>(IterMut<'a, K, V>);
 
 impl<K, V: Debug> Debug for ValuesMut<'_, K, V> {
@@ -394,6 +467,11 @@ impl<'a, K, V> Iterator for ValuesMut<'a, K, V> {
 
 impl<K, V> FusedIterator for ValuesMut<'_, K, V> {}
 
+/// An iterator over the values of a map.
+/// Yields the values as values.
+/// Generated by the [`into_values`] method.
+///
+/// [`into_values`]: VecMap::into_values
 pub struct IntoValues<K, V>(IntoIter<K, V>);
 
 impl<K, V: Debug> Debug for IntoValues<K, V> {
@@ -422,6 +500,11 @@ impl<K, V> Iterator for IntoValues<K, V> {
 
 impl<K, V> FusedIterator for IntoValues<K, V> {}
 
+/// An iterator over the key-value pairs of a map.
+/// Yields the pairs as tuples of references.
+/// Generated by the [`iter`] method.
+///
+/// [`iter`]: VecMap::iter
 pub struct Iter<'a, K, V>(std::slice::Iter<'a, (K, V)>);
 
 impl<K, V> Clone for Iter<'_, K, V> {
@@ -455,6 +538,11 @@ impl<'a, K, V> Iterator for Iter<'a, K, V> {
 
 impl<K, V> FusedIterator for Iter<'_, K, V> {}
 
+/// An iterator over the key-value pairs of a map.
+/// Yields the pairs as tuples of values.
+/// Generated by the [`into_iter`] method.
+///
+/// [`into_iter`]: IntoIterator::into_iter
 pub struct IntoIter<K, V>(std::vec::IntoIter<(K, V)>);
 
 impl<K: Debug, V: Debug> Debug for IntoIter<K, V> {
@@ -481,6 +569,11 @@ impl<K, V> Iterator for IntoIter<K, V> {
 
 impl<K, V> FusedIterator for IntoIter<K, V> {}
 
+/// An iterator over the key-value pairs of a map.
+/// Yields the pairs as tuples of a reference and a mutable reference.
+/// Generated by the [`iter_mut`] method.
+///
+/// [`iter_mut`]: VecMap::iter_mut
 pub struct IterMut<'a, K, V>(std::slice::IterMut<'a, (K, V)>);
 
 impl<K: Debug, V: Debug> Debug for IterMut<'_, K, V> {
@@ -508,6 +601,11 @@ impl<'a, K, V> Iterator for IterMut<'a, K, V> {
 
 impl<K, V> FusedIterator for IterMut<'_, K, V> {}
 
+/// An iterator over the removed key-value pairs of a map.
+/// Yields the pairs as tuples of values.
+/// Generated by the [`drain`] method.
+///
+/// [`drain`]: VecMap::drain
 pub struct Drain<'a, K, V>(std::vec::Drain<'a, (K, V)>);
 
 impl<K: Debug, V: Debug> Debug for Drain<'_, K, V> {
@@ -534,13 +632,21 @@ impl<K, V> Iterator for Drain<'_, K, V> {
 
 impl<K, V> FusedIterator for Drain<'_, K, V> {}
 
+/// A view into a single key-value pair of a map.
+/// Generated by the [`entry`] method.
+///
+/// [`entry`]: VecMap::entry
 #[derive(Debug)]
 pub enum Entry<'a, K, V> {
+    /// The given key was already in the map.
     Occupied(OccupiedEntry<'a, K, V>),
+
+    /// The given key was not in the map.
     Vacant(VacantEntry<'a, K, V>),
 }
 
 impl<'a, K, V> Entry<'a, K, V> {
+    /// Insert a value under the entry’s key if the entry is vacant.
     pub fn or_insert(self, default: V) -> &'a mut V {
         match self {
             Entry::Occupied(o) => o.into_mut(),
@@ -548,6 +654,9 @@ impl<'a, K, V> Entry<'a, K, V> {
         }
     }
 
+    /// Like [`or_insert`], but takes a closure that is only called if the entry is vacant.
+    ///
+    /// [`or_insert`]: Entry::or_insert
     pub fn or_insert_with<F: FnOnce() -> V>(self, default: F) -> &'a mut V {
         match self {
             Entry::Occupied(o) => o.into_mut(),
@@ -555,6 +664,9 @@ impl<'a, K, V> Entry<'a, K, V> {
         }
     }
 
+    /// Like [`or_insert_with`], but the closure takes the entry’s key as an argument.
+    ///
+    /// [`or_insert_with`]: Entry::or_insert_with
     pub fn or_insert_with_key<F: FnOnce(&K) -> V>(self, default: F) -> &'a mut V {
         match self {
             Entry::Occupied(o) => o.into_mut(),
@@ -565,6 +677,7 @@ impl<'a, K, V> Entry<'a, K, V> {
         }
     }
 
+    /// Get a reference to the entry’s key.
     pub fn key(&self) -> &K {
         match self {
             Entry::Occupied(o) => o.key(),
@@ -572,6 +685,7 @@ impl<'a, K, V> Entry<'a, K, V> {
         }
     }
 
+    /// Modify the entry’s value if the entry is occupied.
     pub fn and_modify<F>(self, f: F) -> Self
     where
         F: FnOnce(&mut V),
@@ -588,11 +702,13 @@ impl<'a, K, V> Entry<'a, K, V> {
 }
 
 impl<'a, K, V: Default> Entry<'a, K, V> {
+    /// Insert the default value under the entry’s key if the key is vacant.
     pub fn or_default(self) -> &'a mut V {
         self.or_insert(Default::default())
     }
 }
 
+/// An occupied [`Entry`].
 pub struct OccupiedEntry<'a, K, V> {
     index: usize,
     key: K,
@@ -600,55 +716,69 @@ pub struct OccupiedEntry<'a, K, V> {
 }
 
 impl<'a, K, V> OccupiedEntry<'a, K, V> {
+    /// Get a reference to the entry’s key.
     pub fn key(&self) -> &K {
         &self.key
     }
 
+    /// Remove the key-value pair at the entry’s key.
     pub fn remove_entry(self) -> (K, V) {
         self.map.0.remove(self.index)
     }
 
+    /// Get a reference to the value at the entry’s key.
     pub fn get(&self) -> &V {
         &self.map.0[self.index].1
     }
 
+    /// Get a mutable reference to the value at the entry’s key.
     pub fn get_mut(&mut self) -> &mut V {
         &mut self.map.0[self.index].1
     }
 
+    /// Get a mutable reference to the value at the entry’s key that lives as long as the map.
     pub fn into_mut(self) -> &'a mut V {
         &mut self.map.0[self.index].1
     }
 
+    /// Replace the value at the entry’s key with a new value.
     pub fn insert(&mut self, value: V) -> V {
         std::mem::replace(&mut self.map.0[self.index].1, value)
     }
 
+    /// Remove the key-value pair at the entry’s key.
     pub fn remove(self) -> V {
         self.map.0.remove(self.index).1
     }
 }
 
-impl<K: Debug, V> Debug for OccupiedEntry<'_, K, V> {
+impl<K: Debug, V: Debug> Debug for OccupiedEntry<'_, K, V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_tuple("OccupiedEntry").field(self.key()).finish()
+        f.debug_tuple("OccupiedEntry")
+            .field(self.key())
+            .field(self.get())
+            .finish()
     }
 }
 
+/// A vacant [`Entry`].
 pub struct VacantEntry<'a, K, V> {
     key: K,
     map: &'a mut VecMap<K, V>,
 }
 
 impl<'a, K, V> VacantEntry<'a, K, V> {
+    /// Get a reference to the entry’s key.
     pub fn key(&self) -> &K {
         &self.key
     }
 
+    /// Get the entry’s key.
     pub fn into_key(self) -> K {
         self.key
     }
 
+    /// Insert a value under the entry’s key.
     pub fn insert(self, value: V) -> &'a mut V {
         self.map.0.push((self.key, value));
         &mut self.map.0.last_mut().unwrap().1
